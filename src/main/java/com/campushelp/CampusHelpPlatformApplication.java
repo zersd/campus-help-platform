ba9592eb0115814126b04971f1efe4acf72c4019
zersd <730489447@qq.com>
2026-05-20 14:50:46 +0800

    feat: initialize SpringBoot entry point

---
+++ src/main/java/com/campushelp/CampusHelpPlatformApplication.java
@@ -0,0 +1,11 @@
+package com.campushelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusHelpPlatformApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CampusHelpPlatformApplication.class, args);
+    }
+}
